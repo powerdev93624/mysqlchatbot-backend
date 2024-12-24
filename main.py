871f38c12e225cc47957d5a7759c503583bc50c0
powerdev93624 <michael1993624@gmail.com>
2024-12-24 13:09:00 -0800

    changed model

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def write_query(question):
     # print(structured_llm.invoke(prompt))
 
 if __name__ == "__main__":
-    question = "how many patients are there?"
+    question = "what is hospital name?"
     write_query(question)
     
 
